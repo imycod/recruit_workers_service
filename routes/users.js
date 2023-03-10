f5735f768fc35ccda726c8595847a9055f191ad7
wuxs <317009160@qq.com>
2023-03-10 12:40:19 +0800

    feat:跨域中间件处理

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    console.log('11111111111111111111111')
+  res.send({
+    code:200,
+    data: {
+        name: 'sikonggpw',
+        age: 18
+    }
+  });
 });
 
 module.exports = router;
